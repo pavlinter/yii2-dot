d58dc52ff17e2519e7e7a977caabe5b6fe03eff1
pavlinter <pavlinter@gmail.com>
2015-12-04 16:49:40 +0200

    add new function

--- doT/assets/js/jquery.doT.js
+++ doT/assets/js/jquery.doT.js
@@ -1,8 +1,44 @@
+/**
+ * @package yii2-dot
+ * @author Pavels Radajevs <pavlinter@gmail.com>
+ * @copyright Copyright &copy; Pavels Radajevs <pavlinter@gmail.com>, 2015
+ * @version 1.1.0
+ */
 (function($) {
 
-    $.fn.tmplHtml = function(tmplId, data, prefix) {
-        var p = prefix || 'tmpl_';
+    var default_prefix = 'tmpl_';
+
+    $.tmpl = function(tmplId, data, prefix) {
+        var p = prefix || default_prefix;
         var tmpl = doT.template($('#' + p + tmplId).text());
+        data = $.extend({}, data);
+        if (!$.isArray(data)) data = [data];
+
+        var html = '';
+        for (var itemIdx = 0; itemIdx < data.length; itemIdx++) {
+            html += tmpl(data[itemIdx]);
+        }
+        return html;
+    };
+
+    $.eachTmpl = function(tmplId, arr, prefix) {
+        var p = prefix || default_prefix;
+        var tmpl = doT.template($('#' + p + tmplId).text());
+        var html = '';
+        for (var i in arr) {
+            data = $.extend({}, arr[i]);
+            if (!$.isArray(data)) data = [data];
+            for (var itemIdx = 0; itemIdx < data.length; itemIdx++) {
+                html += tmpl(data[itemIdx]);
+            }
+        }
+        return html;
+    };
+
+    $.fn.tmplHtml = function(tmplId, data, prefix) {
+        var p = prefix || default_prefix;
+        var tmpl = doT.template($('#' + p + tmplId).text());
+        data = $.extend({}, data);
         if (!$.isArray(data)) data = [data];
 
         return this.each(function() {
@@ -15,8 +51,9 @@
     };
 
     $.fn.tmplAppend = function(tmplId, data, prefix) {
-        var p = prefix || 'tmpl_';
+        var p = prefix || default_prefix;
         var tmpl = doT.template($('#' + p + tmplId).text());
+        data = $.extend({}, data);
         if (!$.isArray(data)) data = [data];
 
         return this.each(function() {
@@ -28,8 +65,9 @@
     };
 
     $.fn.tmplPrepend = function(tmplId, data, prefix) {
-        var p = prefix || 'tmpl_';
+        var p = prefix || default_prefix;
         var tmpl = doT.template($('#' + p + tmplId).text());
+        data = $.extend({}, data);
         if (!$.isArray(data)) data = [data];
 
         return this.each(function() {
